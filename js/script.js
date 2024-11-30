df42edf186206d47f3ee8a67aa4541ca3870ae9e
TarasRogiv <r.taras.t.1@gmail.com>
2024-12-01 00:32:44 +0200

    add images, mobile interface to hero, benefits, team section

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 const openButton = document.getElementById("order-button");
 const closeButton = document.getElementById("close-button");
 const backdropWindow = document.querySelector(".backdrop");
-
-// console.log(openButton);
-// console.log(closeButton);
-// console.log(modalWindow);
+// mobile menu
+const openMobilemenu = document.querySelector(".burger-menu-button");
+const closeMobileMenu = document.getElementById("close-menu-button");
+const menuContainer = document.querySelector(".menu-mobile");
 
 openButton.addEventListener("click", () => {
   backdropWindow.classList.toggle("is-open");
@@ -12,4 +12,12 @@ openButton.addEventListener("click", () => {
 
 closeButton.addEventListener("click", () => {
   backdropWindow.classList.toggle("is-open");
+});
+
+openMobilemenu.addEventListener("click", () => {
+  menuContainer.classList.toggle("is-open");
+});
+
+closeMobileMenu.addEventListener("click", () => {
+  menuContainer.classList.toggle("is-open");
 });
